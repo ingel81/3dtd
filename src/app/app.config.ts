f7eb7a0718a0f6107ef6aca696d84bcbb252ded9
ingel81 <ingel81@users.noreply.github.com>
2026-01-27 23:34:41 +0100

    chore: migrate from material-icons to material-symbols
    Replace deprecated material-icons package with material-symbols.
    Configure MAT_ICON_DEFAULT_OPTIONS to use outlined font set.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { MAT_ICON_DEFAULT_OPTIONS } from '@angular/material/icon';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -10,5 +11,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(),
+    {
+      provide: MAT_ICON_DEFAULT_OPTIONS,
+      useValue: { fontSet: 'material-symbols-outlined' },
+    },
   ],
 };
